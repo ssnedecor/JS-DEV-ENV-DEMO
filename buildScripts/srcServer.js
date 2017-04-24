1cf0ea89aafc7fc0d12871c05563b6d5c57c73a2
ssnedecor <ssnedecor@bellsouth.net>
2017-04-23 20:33:55 -0400

    transpiler and bundler

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,8 +2,26 @@ var express = require('express');
 var path = require('path');
 var open = require('open');
 
-var port = 3000;
-var app = express();
+//add bundler webpack
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+//ES5 native node
+//var port = 3000;
+//var app = express();
+
+
+//ES6 requires Babel
+const port = 3000;
+const app = express();
+
+//webpack stuff
+const compiler = webpack(config);
+
+app.use ( require('webpack-dev-middleware')(compiler,{
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req,res){
   res.sendFile(path.join(__dirname,'../src/index.html'));
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,6 @@
+//var chalk = require('chalk');  //ES5
+
+import chalk from 'chalk'; //ES6 - node doesn't understand. Reason for babel.
+
+
+console.log(chalk.green('Starting app in dev mode...'));
